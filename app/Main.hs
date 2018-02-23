56d279d1359e0c185a802e9fea061def94bec87b
Marisa Kirisame <lolisa@marisa.moe>
2018-02-23 10:32:04 -0800

    implement widget

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import qualified Lib as Reflex
+
+import qualified Graphics.UI.FLTK.LowLevel.FL as FL
+import Graphics.UI.FLTK.LowLevel.Fl_Types
+import Graphics.UI.FLTK.LowLevel.FLTKHS
+
+buttonCb :: Ref Button -> Reflex.Widget ()
+buttonCb b' = Reflex.Widget $ do
+  l' <- getLabel b'
+  if (l' == "Hello world")
+    then setLabel b' "Goodbye world"
+    else setLabel b' "Hello world"
+
+ui :: Reflex.Widget ()
+ui = Reflex.Widget $ do
+ window <- windowNew
+           (Size (Width 115) (Height 100))
+           Nothing
+           Nothing
+ begin window
+ b' <- buttonNew
+        (Rectangle (Position (X 10) (Y 30)) (Size (Width 95) (Height 30)))
+        (Just "Hello world")
+ setLabelsize b' (FontSize 10)
+ setCallback b' (Reflex.unWidget . buttonCb)
+ end window
+ showWidget window
 
 main :: IO ()
-main = someFunc
+main = Reflex.runWidget ui
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE GeneralizedNewtypeDeriving, OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import qualified Graphics.UI.FLTK.LowLevel.FL as FL
+
+newtype Widget a = Widget { unWidget :: IO a } deriving (Functor, Applicative, Monad)
+
+runWidget :: Widget () -> IO ()
+runWidget (Widget a) = a >> FL.run >> FL.flush
+
+runWidgetRepl :: Widget () -> IO ()
+runWidgetRepl (Widget a) = a >> FL.replRun
